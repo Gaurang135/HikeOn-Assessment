33e22ffad10d88d36fe1f1b98e796e2b6b14da6e
Gaurang Sharma <118263179+Gaurang135@users.noreply.github.com>
2023-12-22 15:07:16 +0530

    Initial Commit

---
+++ src/main/java/com/assessment/hikeOn/HikeOnApplication.java
@@ -0,0 +1,13 @@
+package com.assessment.hikeOn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikeOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HikeOnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assessment/hikeOn/HikeOnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessment.hikeOn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HikeOnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
